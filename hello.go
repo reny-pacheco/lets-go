793981c88a2c7826105d75a3729a9ed1df59ea06
reny <renypacheco08.rp@gmail.com>
2023-02-12 19:22:33 +0800

    feat: data types

--- hello.go
+++ hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var a bool = true    // Boolean
+	var b int = 5        // Integer
+	var c float32 = 3.14 // Floating point number
+	var d string = "Hi!" // String
+
+	// can assign values without the var keyword inside a function only
+	age := 14
+
+	fmt.Println("Boolean: ", a)
+	fmt.Println("Integer: ", b)
+	fmt.Println("Float:   ", c)
+	fmt.Println("String:  ", d)
+	fmt.Println("Age: ", age)
 }
