f4720005dccb108a24846d67fa003bdd050db067
reny <renypacheco08.rp@gmail.com>
2023-02-23 18:26:39 +0800

    feat: update functions

--- main.go
+++ main.go
@@ -8,6 +8,49 @@ func myMessage() {
 	fmt.Println("I just got executed!")
 }
 
+func funcStr(name string) string {
+	return "Hello Guys"
+}
+
+func addNums(num1, num2 int) int {
+	return num1 + num2
+}
+
+func showName(name string) string {
+	return "Hello"
+}
+
+// declaring a type
+type IZ int
+
+// declaring multiple types
+type (
+	IT  int
+	STR string
+)
+
+var name STR = "Reny"
+var amount IT = 1200
+
+// using constants as enums
+const (
+	Male   = 0
+	Female = 1
+)
+
+// auto increment in enums
+const (
+	a = iota
+	b
+	c
+)
+
 func main() {
 	myMessage() // call the function
+	fmt.Println(showName("hello"))
+	fmt.Println(funcStr("hello"))
+	fmt.Println(addNums(2, 3))
+	fmt.Println(name, amount)
+	fmt.Println(Male)
+	fmt.Println(b)
 }
